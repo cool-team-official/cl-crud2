3a5ee2b3ae89f85134917ca43a1c58c6059aa686
icssoa <615206459@qq.com>
2020-12-19 17:01:54 +0800

    优化 el-message-box 显示

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as comps from "./components";
 require("./common");
 
 export const CRUD = {
-	version: "0.3.0",
+	version: "0.3.1",
 
 	install: function (Vue, options) {
 		const { crud, components, plugins } = options || {};
